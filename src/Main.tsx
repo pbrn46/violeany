e2dfeb34166aa0eef4cf9cddbec705ed7e6713ad
Boris Wong <bobobwong@gmail.com>
2022-12-15 18:53:53 -0800

    Adjust gap

--- src/Main.tsx
+++ src/Main.tsx
@@ -8,7 +8,7 @@ export function Main() {
   useViolinPlayer()
   return <>
     <Header />
-    <div className="tw-container tw-mx-auto tw-mt-2">
+    <div className="tw-container tw-mx-auto tw-mt-2 tw-grid tw-gap-3">
       <div className="tw-grid sm:tw-flex tw-grid-cols-12 tw-gap-3">
         <div className="tw-order-1 sm:tw-order-12 tw-col-span-12 sm:tw-flex-grow tw-flex tw-flex-col tw-gap-3">
           <div className="">
